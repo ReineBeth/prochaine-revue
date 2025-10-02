83e2e9c971c45b1d7d573c5b3cc1a45ece6dfe57
ReineBeth <simplement.eli@hotmail.com>
2025-10-01 23:57:25 -0400

    ajout institution

--- functions.php
+++ functions.php
@@ -956,4 +956,34 @@ function expose_acf_fields_to_rest() {
     }
 }
 add_action('rest_api_init', 'expose_acf_fields_to_rest');
+
+function create_auteurs_taxonomy_acf_fields() {
+    if( function_exists('acf_add_local_field_group') ):
+    acf_add_local_field_group(array(
+        'key' => 'group_pr_auteurs_taxonomy',
+        'title' => 'Informations Auteur',
+        'fields' => array(
+            array(
+                'key' => 'field_auteur_institution',
+                'label' => 'Institution',
+                'name' => 'auteur_institution',
+                'type' => 'text',
+                'required' => 0,
+                'show_in_rest' => 1,
+                'instructions' => 'Institution d\'affiliation de l\'auteur (ex: Université de Montréal, UQAM, etc.)'
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'taxonomy',
+                    'operator' => '==',
+                    'value' => 'pr-auteurs',
+                ),
+            ),
+        ),
+    ));
+    endif;
+}
+add_action('acf/init', 'create_auteurs_taxonomy_acf_fields');
 ?>
